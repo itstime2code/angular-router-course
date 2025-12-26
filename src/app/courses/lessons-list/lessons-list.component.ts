6d512dce71a33bfb9d7024b5217795ea793ddf84
prasadmedisetti <medisetti.prasad.cse@gmail.com>
2025-12-26 20:18:46 +0530

    Added navigation to lesson details page

--- src/app/courses/lessons-list/lessons-list.component.ts
+++ src/app/courses/lessons-list/lessons-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, RouterLink } from "@angular/router";
 import { LessonSummary } from "../model/lesson-summary";
 import { NgFor, NgIf } from '@angular/common';
 
@@ -7,7 +7,7 @@ import { NgFor, NgIf } from '@angular/common';
   selector: 'lessons-list',
   templateUrl: './lessons-list.component.html',
   styleUrls: ['./lessons-list.component.css'],
-  imports: [NgFor, NgIf]
+  imports: [NgFor, NgIf, RouterLink]
 })
 export class LessonsListComponent implements OnInit {
 
